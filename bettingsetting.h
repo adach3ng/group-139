33b383209dbfd510d21850f3fefc5eb5765b491f
MagicLolichop <125712480+MagicLolichop@users.noreply.github.com>
2025-04-27 01:02:34 +0800

    Create bettingsetting.h

---
+++ bettingsetting.h
@@ -0,0 +1,24 @@
+// bet_system.h
+#pragma once
+#include <iostream>
+
+class BetSystem {
+private:
+    int baseBet;    // base betting amount
+    float multiplier; // dynamic reward ratio
+
+public:
+    BetSystem(int defaultBet = 10);
+    
+    // set betting amount
+    bool placeBet(int& balance, int amount);
+    
+    // calculate actural rewatd
+    int calculatePayout(int basePayout) const;
+    
+    // get current betting ratio
+    float getMultiplier() const;
+    
+    // show the instruction
+    void showBetHelp() const;
+};
